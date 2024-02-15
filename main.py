eba35b3256b92048e624acdfc441584c0a389203
jackrose27 <jack.rosenblum@students.fhu.edu>
2024-02-15 15:21:35 -0600

    Update main.py
    I believe this is the right code.

--- main.py
+++ main.py
@@ -2,14 +2,22 @@ class Queue():
     def __init__(self):
         self.cards = []
 
-    #REMOVE PASS AND COMPLETE THE FUNCTION
+    
     def push(self, card):
-        pass
+        self.cards.append(card)
 
     #REMOVE PASS AND COMPLETE THE FUNCTION
     def pop(self):
-        pass
+        del self.cards[0]
+        
+        
 
 if __name__ == '__main__':
     #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+    deck = Queue()
+    deck.push(3)
+    deck.push(7)
+    deck.push(8)
+    deck.pop()
+    print(deck.cards)
+    
